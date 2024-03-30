b92eb31ae81b4123222cbec7e9d3b0b0797a89a1
YaBoiChips <63213388+YaBoiChips@users.noreply.github.com>
2024-03-30 00:48:11 -0600

    init

---
+++ src/main/java/yaboichips/blockroyale/BlockRoyale.java
@@ -0,0 +1,10 @@
+package yaboichips.blockroyale;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BlockRoyale implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/yaboichips/blockroyale/client/BlockRoyaleClient.java
@@ -0,0 +1,10 @@
+package yaboichips.blockroyale.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockRoyaleClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
